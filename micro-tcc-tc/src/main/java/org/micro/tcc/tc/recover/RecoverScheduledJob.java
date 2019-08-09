61abdec95c8990fe833c3e881394c05c3e564368
liuzhijian <liuzhijian@rjmart.cn>
2019-08-09 18:52:41 +0800

    修改

---
+++ micro-tcc-tc/src/main/java/org/micro/tcc/tc/recover/RecoverScheduledJob.java
@@ -0,0 +1,59 @@
+package org.micro.tcc.tc.recover;
+
+import lombok.extern.slf4j.Slf4j;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerFactory;
+import org.quartz.impl.StdSchedulerFactory;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.scheduling.quartz.CronTriggerFactoryBean;
+import org.springframework.scheduling.quartz.MethodInvokingJobDetailFactoryBean;
+
+
+@Slf4j
+public class RecoverScheduledJob implements ApplicationRunner {
+
+    private TransactionRecovery transactionRecovery=new TransactionRecovery();
+
+    private TransactionRecoverConfig transactionConfigurator=TransactionRecoverConfig.getInstance();
+
+    private Scheduler scheduler;
+
+    public void init() {
+
+        try {
+            MethodInvokingJobDetailFactoryBean jobDetail = new MethodInvokingJobDetailFactoryBean();
+            jobDetail.setTargetObject(transactionRecovery);
+            jobDetail.setTargetMethod("beginRecover");
+            jobDetail.setName("transactionRecoveryJob");
+            jobDetail.setConcurrent(false);
+            jobDetail.afterPropertiesSet();
+
+            CronTriggerFactoryBean cronTrigger = new CronTriggerFactoryBean();
+            cronTrigger.setBeanName("transactionRecoveryCronTrigger");
+            cronTrigger.setCronExpression(transactionConfigurator.getCronExpression());
+            cronTrigger.setJobDetail(jobDetail.getObject());
+            cronTrigger.afterPropertiesSet();
+            SchedulerFactory schedulerfactory = new StdSchedulerFactory();
+            scheduler=schedulerfactory.getScheduler();
+            scheduler.scheduleJob(jobDetail.getObject(), cronTrigger.getObject());
+
+            scheduler.start();
+            log.info("TCC:开启分布式事务定时任务");
+
+        } catch (Exception e) {
+            log.error("TCC:定时commit/cancel事务发生异常！",e);
+        }
+    }
+
+    public void setTransactionRecovery(TransactionRecovery transactionRecovery) {
+        this.transactionRecovery = transactionRecovery;
+    }
+
+
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        init();
+    }
+}
---
+++ micro-tcc-tc/src/main/java/org/micro/tcc/tc/recover/TransactionRecoverConfig.java
@@ -0,0 +1,59 @@
+package org.micro.tcc.tc.recover;
+
+
+
+import java.net.SocketTimeoutException;
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class TransactionRecoverConfig implements RecoverConfig {
+
+    public static final TransactionRecoverConfig INSTANCE = new TransactionRecoverConfig();
+
+    private int maxRetryCount = 30;
+
+    private int recoverDuration = 160; //160 seconds
+
+    private String cronExpression = "0 */1 * * * ?";
+
+    public static TransactionRecoverConfig getInstance(){
+        return INSTANCE;
+    }
+
+
+
+    public TransactionRecoverConfig() {
+
+    }
+
+    @Override
+    public int getMaxRetryCount() {
+        return maxRetryCount;
+    }
+
+    @Override
+    public int getRecoverDuration() {
+        return recoverDuration;
+    }
+
+    @Override
+    public String getCronExpression() {
+        return cronExpression;
+    }
+
+
+    public void setMaxRetryCount(int maxRetryCount) {
+        this.maxRetryCount = maxRetryCount;
+    }
+
+    public void setRecoverDuration(int recoverDuration) {
+        this.recoverDuration = recoverDuration;
+    }
+
+    public void setCronExpression(String cronExpression) {
+        this.cronExpression = cronExpression;
+    }
+
+
+}
